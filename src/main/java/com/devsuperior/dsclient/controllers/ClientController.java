6ba368ef911a5cc5e0c462b3dedaa992bf5791bc
Lucas Ramos <lucas.rsilva32@gmail.com>
2023-11-10 20:42:07 -0300

    GET Clients by id / Clients paged

---
+++ src/main/java/com/devsuperior/dsclient/controllers/ClientController.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsclient.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ClientDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+    
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+    }
+
+ 
+}
--- src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -16,10 +16,13 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
     private String cpf;
     private Double income;
+    
     private LocalDate birthDate;
+    
     private Integer children;
 
     public Client(){
