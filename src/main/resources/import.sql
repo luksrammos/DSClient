f34a991c7afbace47ed6df303b36778e29c2cdd7
Lucas Ramos <lucas.rsilva32@gmail.com>
2023-11-10 19:45:48 -0300

    database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Lucas','41754787464', 5000.00, '1993-02-15', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Nicolas','51454787462', 2000.00, '2000-09-05', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Mariana','32654787461', 3000.00, '1985-07-20', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Gabriel','22854787463', 4000.00, '1990-11-10', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Juliana','12354787465', 3500.00, '1988-04-03', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Rafael','94354787467', 4500.00, '1983-09-12', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Camila','74554787469', 3200.00, '1992-06-25', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Matheus','62354787470', 2800.00, '1995-03-18', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Isabela','81754787471', 3800.00, '1987-12-07', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES('Diego','96554787472', 4200.00, '1984-08-30', 3);
