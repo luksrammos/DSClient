c298b29a7bde8d9f34ab65b3f93e8dd092ec055b
Lucas Ramos <lucas.rsilva32@gmail.com>
2023-11-12 17:31:28 -0300

    PUT client

--- src/main/java/com/devsuperior/dsclient/controllers/ClientController.java
+++ src/main/java/com/devsuperior/dsclient/controllers/ClientController.java
@@ -1,14 +1,19 @@
 package com.devsuperior.dsclient.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.services.ClientService;
@@ -21,18 +26,29 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping(value = "/{id}")
-    public ClientDTO findById(@PathVariable Long id){
-        return service.findById(id);
+    public ResponseEntity <ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
     
     @GetMapping
-    public Page<ClientDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
       @PostMapping
-        public ClientDTO insert(@RequestBody ClientDTO dto){
-            return service.insert(dto);
+        public ResponseEntity <ClientDTO> insert(@RequestBody ClientDTO dto){
+            dto = service.insert(dto);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+            return ResponseEntity.created(uri).body(dto);
         }
  
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
 }
